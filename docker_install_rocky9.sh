687aa54baf1537f34046c4be5a84d2e1cf4d1b27
kymotsu <ustomyk@gmail.com>
2025-05-16 16:44:54 -0500

    Update docker_install_rocky9.sh

--- docker_install_rocky9.sh
+++ docker_install_rocky9.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+
+dnf install -y dnf-plugins-core
+dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+systemctl start docker
+systemctl enable docker
