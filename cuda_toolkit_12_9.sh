640cdc600eaf437be24e5431ba00b64862018a5d
kymotsu <ustomyk@gmail.com>
2025-05-16 18:02:45 -0500

    Update cuda_toolkit_12_9.sh

--- cuda_toolkit_12_9.sh
+++ cuda_toolkit_12_9.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+read -p "Working as of 5/16/2025 - Press Enter to Continue"
+
 sudo apt install build-essential -y
 
 wget https://developer.download.nvidia.com/compute/cuda/12.9.0/local_installers/cuda_12.9.0_575.51.03_linux.run
